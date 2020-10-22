06235efa613ab8e2604446108457741e04dc7310
Markus Opolka <markus@martialblog.de>
2020-10-22 07:38:06 +0200

    Add more aliases

--- files/profile.defaults.sh
+++ files/profile.defaults.sh
@@ -2,5 +2,9 @@
 # Contains some shared shell defaults
 
 # Aliases
+alias gst='git status'
+alias la='ls -lAh'
+alias ll='ls -lh'
 alias puppet-deploy='g10k -maxworker 2 -config /etc/g10k/g10k.yaml'
 alias puppet-apply='puppet apply /etc/puppetlabs/puppet/site.pp'
+alias apt-upgrade='apt-get update && apt-get upgrade && apt-get autoremove && apt-get clean'
