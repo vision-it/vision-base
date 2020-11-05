a1b5e93c4c257f005a576102c18736d7a848e594
Markus Opolka <opolkams@iis.fraunhofer.de>
2020-11-05 13:18:54 +0100

    Add new aliases

--- files/profile.defaults.sh
+++ files/profile.defaults.sh
@@ -2,7 +2,9 @@
 # Contains some shared shell defaults
 
 # Aliases
+alias _='sudo'
 alias gst='git status'
+alias gco='git checkout'
 alias la='ls -lAh'
 alias ll='ls -lh'
 alias puppet-deploy='g10k -maxworker 2 -config /etc/g10k/g10k.yaml'
